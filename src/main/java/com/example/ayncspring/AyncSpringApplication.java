405fa719eb05b860ca2621f1ae35252f27f5b400
Miewone <dlsrk489@gmail.com>
2021-06-13 16:58:54 +0900

    git init

---
+++ src/main/java/com/example/ayncspring/AyncSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.ayncspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyncSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AyncSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ayncspring/AyncSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ayncspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AyncSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
